84e626d6883b0b4824dc76e26ccbfd11348ed304
Arshad Siddiqui <dsmail.alok@gmail.com>
2020-09-23 09:48:22 +0530

    changes

---
+++ precache-manifest.b8face9c34e4c695b4f9a7134f0e318f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199c60557065b168da060849618c4c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb417efda9dd6665aadc",
+    "url": "/static/css/3.bb5271d0.chunk.css"
+  },
+  {
+    "revision": "65e90df4c1352a50e2f4",
+    "url": "/static/css/4.88cba393.chunk.css"
+  },
+  {
+    "revision": "0c53a04a26190fe86440",
+    "url": "/static/css/5.b9d87a31.chunk.css"
+  },
+  {
+    "revision": "f15603f5f745cac0819e",
+    "url": "/static/css/6.9e083680.chunk.css"
+  },
+  {
+    "revision": "a9e800e1acf1ab986ebc",
+    "url": "/static/css/main.88d3ee01.chunk.css"
+  },
+  {
+    "revision": "f04993eab8e3f2c1ac8b",
+    "url": "/static/js/0.d2a32d0e.chunk.js"
+  },
+  {
+    "revision": "064f9da3319943ca1b2eabf089e3b976",
+    "url": "/static/js/0.d2a32d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb417efda9dd6665aadc",
+    "url": "/static/js/3.59299fe4.chunk.js"
+  },
+  {
+    "revision": "26aa19e3344774568c18d9d87c8757a6",
+    "url": "/static/js/3.59299fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e90df4c1352a50e2f4",
+    "url": "/static/js/4.73507d9f.chunk.js"
+  },
+  {
+    "revision": "0c53a04a26190fe86440",
+    "url": "/static/js/5.afc41ccc.chunk.js"
+  },
+  {
+    "revision": "f15603f5f745cac0819e",
+    "url": "/static/js/6.da0ba549.chunk.js"
+  },
+  {
+    "revision": "48763b73d8c195527ebddd5857148be0",
+    "url": "/static/js/6.da0ba549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e800e1acf1ab986ebc",
+    "url": "/static/js/main.93bbdd60.chunk.js"
+  },
+  {
+    "revision": "ac9b87d48a3b6c06a637",
+    "url": "/static/js/runtime-main.a57084fb.js"
+  },
+  {
+    "revision": "0087dce419a7376eb06c837f740d31eb",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.0087dce4.woff"
+  },
+  {
+    "revision": "089ab3c11c572362d088083c561cfa55",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.089ab3c1.eot"
+  },
+  {
+    "revision": "1bc3764815bb391a16771c42c6e460ec",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.1bc37648.ttf"
+  },
+  {
+    "revision": "4b57c6a5567c9ef09913983f82904faa",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.4b57c6a5.svg"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2a103029dda6ab1bc9c8b0ee72536e41",
+    "url": "/static/media/Simple-Line-Icons.2a103029.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3702b57421b113197505002ce53896fb.js"
+  "/precache-manifest.b8face9c34e4c695b4f9a7134f0e318f.js"
 );
 
 self.addEventListener('message', (event) => {
